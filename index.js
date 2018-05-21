b540393ef8ebde9cd744a06307d4cb3e4bc4b946
许庆钢 <qinggangxu@sohu-inc.com>
2018-05-21 15:41:28 +0800

    improve

--- index.js
+++ index.js
@@ -30,9 +30,21 @@ module.exports = {
     ],
 
     rules: {
-        indent: ['error', 4],
+        indent: ['error', 4, { SwitchCase: 2 }],
+        'max-len': ['error', { code: 120 }],
+        'no-use-before-define': ['error', { functions: false, classes: false }],
+        // enforce variables to be declared either together or separately
+        // 'one-var': ['error', { var: 'consecutive', let: 'consecutive', const: 'consecutive' }],
+        'one-var': 'off',
+
+        // import
+        // 'import/first': ['error', 'absolute-first'],
+        'import/first': 'off',
+        'import/order': 'off',
+        'import/prefer-default-export': 'off',
 
         // react
+        'react/no-danger': 'off',
         // 缩进
         'react/jsx-indent': ['error', 4],
         // react 属性缩进
@@ -43,6 +55,8 @@ module.exports = {
         // Prevent missing React when using JSX
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': ['error', { ignore: ['dispatch'], customValidators: [] }],
+        // Prevent multiple component definition per file
+        'react/no-multi-comp': 'off',
         // 修改react 内部方法属性排序
         'react/sort-comp': [
             1,
@@ -81,8 +95,6 @@ module.exports = {
         // Enforce label tags have associated control.
         'jsx-a11y/label-has-for': 'off',
         'jsx-a11y/anchor-is-valid': 'off',
-        'import/first': ['error', 'DISABLE-absolute-first'],
-        'import/prefer-default-export': 'off',
 
         // regular
         // 尾随逗号
@@ -93,12 +105,17 @@ module.exports = {
             exports: 'always-multiline',
             functions: 'always-multiline',
         }],
-
-        // Quoting Style for Property Names
-        'quote-props': [2, 'as-needed', { numbers: true }],
-
-        // enforce variables to be declared either together or separately
-        'one-var': 'off',
+        // 对象属性是否换行
+        'object-curly-newline': ['error', { consistent: true }],
+        // 优先解构
+        // 'prefer-destructuring': ['error', { object: false, array: false }],
+        'prefer-destructuring': 'off',
+        'no-shadow': ['error', { hoist: 'never' }],
+        'no-plusplus': 'off',
+        // Disallow Dangling Underscores in Identifiers
+        'no-underscore-dangle': ['error', { allowAfterThis: true }],
+        'quote-props': ['error', 'as-needed', { numbers: true }],
+        'one-var-declaration-per-line': [2, 'initializations'],
 
         // 括号前后一致
         'function-paren-newline': ['error', 'consistent'],
@@ -106,16 +123,8 @@ module.exports = {
         // 允许三元表达式和与或操作符表达式
         'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
 
-        // Disallow Dangling Underscores in Identifiers
-        'no-underscore-dangle': [2, { allowAfterThis: true }],
-
-        // 对象属性是否换行
-        // 'object-curly-newline': ['error', 'never'],
-
         // allaw href="javascript:xxx"
         'no-script-url': 'off',
-
-        'prefer-destructuring': ['error', { object: false, array: false }],
 
         'no-param-reassign': [
             'warn',
@@ -138,11 +147,11 @@ module.exports = {
     },
 
     // eslint-import-resolver-webpack 让eslint，使用webpack的resolver来检查package是否引用正确
-    // settings: {
-    //     'import/resolver': {
-    //         webpack: {
-    //             config: 'webpack.config.js',
-    //         },
-    //     },
-    // },
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'webpack.config.js',
+            },
+        },
+    },
 };
